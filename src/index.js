0800f94789c5cea683fb60a7595ee56218964117
quyhochanh148 <quytthe173122@fpt.edu.vn>
2024-06-10 21:57:43 +0700

    SCRUM-11 home-details-login

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import Details from './compoment/Detail';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Login/>
-   {/* <Home/> */}
+   {/* <Login/> */}
+   <Home/>
    {/* <Details/> */}
   </React.StrictMode>
 );
