f0b825c2314831d2ee467ead73e0db7b29a21979
quyhochanh148 <quytthe173122@fpt.edu.vn>
2024-06-16 22:23:36 +0700

    SCRUM-12 create-new-admin

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 import Login from './compoment/login';
 import Home from './compoment/Home';
 import Details from './compoment/Detail';
+import Admin from './compoment/admin';
+import Admin1 from './compoment/admin1';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <Login/> */}
+   {/* <Login/>
    <Home/>
-   {/* <Details/> */}
+   <Details/> */}
+   {/* <Admin/> */}
+   {/* <Admin1/> */}
   </React.StrictMode>
 );
